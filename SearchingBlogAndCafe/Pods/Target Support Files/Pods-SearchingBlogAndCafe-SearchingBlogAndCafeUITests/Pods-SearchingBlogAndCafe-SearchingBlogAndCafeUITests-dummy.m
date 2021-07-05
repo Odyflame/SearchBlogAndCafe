0a96cf0539cd11a2e86fb6c7ab0fae9d7faad32f
hyeonjae Lee <odyflame@icloud.com>
2021-07-05 18:35:43 +0900

    [ Refactor ] Refactor pods

---
+++ SearchingBlogAndCafe/Pods/Target Support Files/Pods-SearchingBlogAndCafe-SearchingBlogAndCafeUITests/Pods-SearchingBlogAndCafe-SearchingBlogAndCafeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SearchingBlogAndCafe_SearchingBlogAndCafeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SearchingBlogAndCafe_SearchingBlogAndCafeUITests
+@end
---
+++ SearchingBlogAndCafe/Pods/Target Support Files/Pods-SearchingBlogAndCafeTests/Pods-SearchingBlogAndCafeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SearchingBlogAndCafeTests : NSObject
+@end
+@implementation PodsDummy_Pods_SearchingBlogAndCafeTests
+@end
