fbfed14908197caf65deb4ab4a77dc6344db7ac1
hyeonjae Lee <odyflame@icloud.com>
2021-07-05 17:59:28 +0900

    [ Feature ] Create Project and init pods

---
+++ SearchingBlogAndCafe/SearchingBlogAndCafe/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SearchingBlogAndCafe
+//
+//  Created by apple on 2021/07/05.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ SearchingBlogAndCafe/SearchingBlogAndCafe/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SearchingBlogAndCafe
+//
+//  Created by apple on 2021/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
